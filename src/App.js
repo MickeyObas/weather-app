0d4439a2ad583999890ddff3cab145f8b38bfd96
Michael Goke <mikhzobby@gmail.com>
2024-07-24 02:06:21 +0100

    all done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import './App.css';
 import CityInput from './components/CityInput';
 import ForecastBox from './components/ForecastBox';
-import sun from './images/sun.png';
+
+import { useState } from 'react';
+
 
 function App() {
+
+  const [query, setQuery] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const API_KEY = '805c4656da8040839e8115402242307';
+
+  function handleChange(e){
+    setQuery(e.target.value);
+  }
+
+  function handleClick(){
+    fetchWeather();
+  }
+
+  const fetchWeather = async () =>  {
+    console.log(query);
+    setLoading(true);
+    setError(null)
+    try {
+      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${query}&days=5&aqi=no&alerts=no`);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch weather data');
+      }
+      const data = await response.json();
+      setWeather(data);
+    } catch(err){
+      console.log(err);
+      setError(err.message);
+    } finally{
+      setLoading(false);
+    }
+  }
+
   return (
     <>
-      <CityInput />
-      <ForecastBox />
+      <CityInput
+        onChange={handleChange}
+        onClick={handleClick}
+       />
+       {loading && <p>Loading...</p>}
+       {error && <p style={{color:'red'}}>Error: {error}</p>}
+      <ForecastBox 
+        weather={weather}
+      />
     </>
   )
 }
--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -1,7 +1,15 @@
-export default function CityInput(){
+export default function CityInput({onChange, onClick}){
     return (
         <div id='cityContainer'>
-            <input name='city' id='cityInput' placeholder='Enter a city...'/>
+            <input 
+            name='city'
+            id='cityInput'
+            placeholder='Enter a city...'
+            onChange={onChange}
+            />
+            <button
+            onClick={onClick}
+            >Get Forecast</button>
         </div>
     )
 }
